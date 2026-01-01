62de3ac2f6b7ee2974587df6d1314759e02c7b84
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-01-01 15:38:59 +0530

    I created a base file to export every single utility

---
+++ server/src/utils/index.utils.js
@@ -0,0 +1,10 @@
+/* ---------------------------------------------------------------------------------------
+index.utils.js
+This is a centralized exporting file for every single utility
+------------------------------------------------------------------------------------------ */
+
+import ApiResponse from "./apiResponse";
+import ApiError from "./apiError";
+import asyncHandler from "./asyncHandler";
+
+export { ApiResponse, ApiError, asyncHandler };
