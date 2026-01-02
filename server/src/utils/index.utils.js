9c3b8d39369220aec169c9d6084ad42bf9fe8c51
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-01-02 12:54:24 +0530

    Created a utility function to generate the random string for refresh tokens. Categorized the utilities into different folders

--- server/src/utils/index.utils.js
+++ server/src/utils/index.utils.js
@@ -3,9 +3,16 @@ index.utils.js
 This is a centralized exporting file for every single utility
 ------------------------------------------------------------------------------------------ */
 
-import ApiResponse from "./apiResponse";
-import ApiError from "./apiError";
-import asyncHandler from "./asyncHandler";
-import mailSender from "./mailSender";
+import ApiResponse from "./api/apiResponse";
+import ApiError from "./api/apiError";
+import asyncHandler from "./errorHandler/asyncHandler";
+import mailSender from "./mailer/mailSender";
+import generateRefreshTokenString from "./tokens/generateRefreshTokenString";
 
-export { ApiResponse, ApiError, asyncHandler, mailSender };
+export {
+  ApiResponse,
+  ApiError,
+  asyncHandler,
+  mailSender,
+  generateRefreshTokenString,
+};
