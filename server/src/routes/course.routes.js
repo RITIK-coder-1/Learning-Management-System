6830cb2a70777cfc5e62692d1183b1956ffe706c
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-01-08 13:27:45 +0530

    Defined the route for creating courses

--- server/src/routes/course.routes.js
+++ server/src/routes/course.routes.js
@@ -5,12 +5,17 @@ This file handles all the course related routes
 
 import { Router } from "express";
 import { verifyJwt, isInstructor } from "../middleware/index.middleware.js";
+import { createCourse } from "../controllers/course.controllers.js";
 
 const router = Router();
 
 /* ---------------------------------------------------------------------------------------
 SPECIFIC ROUTES:
-
+- create a course
 ------------------------------------------------------------------------------------------ */
+
+router
+  .route("/dashboard/courses/create")
+  .post(verifyJwt, isInstructor, createCourse);
 
 export { router as courseRouter };
