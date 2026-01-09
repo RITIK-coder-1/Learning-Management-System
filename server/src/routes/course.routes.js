27a4a10c144ea954d8ba91ac37c730e412f9e794
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-01-09 13:39:13 +0530

    Defined the route to add the course video

--- server/src/routes/course.routes.js
+++ server/src/routes/course.routes.js
@@ -9,17 +9,24 @@ import {
   isInstructor,
   upload,
 } from "../middleware/index.middleware.js";
-import { createCourse } from "../controllers/course.controllers.js";
+import {
+  addCourseVideo,
+  createCourse,
+} from "../controllers/course.controllers.js";
 
 const router = Router();
 
 /* ---------------------------------------------------------------------------------------
 SPECIFIC ROUTES:
 - create a course
+- add a video 
 ------------------------------------------------------------------------------------------ */
 
 router
   .route("/dashboard/courses/create")
   .post(verifyJwt, isInstructor, upload.single("thumbnail"), createCourse);
+router
+  .route("/dashboard/courses/:courseId/update")
+  .post(verifyJwt, isInstructor, upload.single("courseVideo"), addCourseVideo);
 
 export { router as courseRouter };
