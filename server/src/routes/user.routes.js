fffcb980a08bc7c92584a0ea9837aa11d836944c
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-01-16 16:55:39 +0530

    updated my user API endpoints to follow the REST patterns

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -30,33 +30,37 @@ SPECIFIC ROUTES:
 - OTP validation and register route
 - Create OTP for login route 
 - OTP validation and login route
+- New Token 
 - Logout user
-- Update the user's details or delete the profile pic
+- Get the user's profile, update and delete it. Get an OTP for email update. 
+- Update the email by validating the OTP 
+- Delete the user profile picture
 - Update the user's password
-- Create OTP for email update route 
-- Update the user email 
-- Delete the user
 ------------------------------------------------------------------------------------------ */
 
 // PUBLIC ROUTES
 
-router.route("/register").post(upload.single("profilePic"), createRegisterOtp); // validate data and generate an OTP
-router.route("/register/otp").post(registerUser); // validate the OTP and register the user
-router.route("/login").post(createLoginOtp); // validate data and generate an OTP
-router.route("/login/otp").post(loginUser); // validate the OTP and login the user
+router
+  .route("/register-otp")
+  .post(upload.single("profilePic"), createRegisterOtp); // validate data and generate an OTP
+router.route("/register").post(registerUser); // validate the OTP and register the user
+router.route("/login-otp").post(createLoginOtp); // validate data and generate an OTP
+router.route("/login").post(loginUser); // validate the OTP and login the user
 router.route("/token").post(newAccessToken); // generate a new access token after expiration
 
 // SECURED ROUTES
 
 router.route("/logout").post(verifyJwt, logoutUser); // log the user out
-router.route("/profile").get(verifyJwt, getUser); // getting a user's details
 router
-  .route("/profile/details")
+  .route("/profile")
+  .get(verifyJwt, getUser) // getting a user's details
   .patch(verifyJwt, updateUserDetails) // updating a user's details
-  .delete(verifyJwt, deleteProfilePic); // deleting a user's profile pic (only for students)
+  .delete(verifyJwt, deleteUserAccount); // deleting the user
+router
+  .route("/profile/email")
+  .post(verifyJwt, createUpdateEmailOtp) // validate data and generate an OTP
+  .patch(verifyJwt, updateEmail); // validate OTP and update the email
+router.route("/profile/pic").delete(verifyJwt, deleteProfilePic); // deleting a user's profile pic (only for students)
 router.route("/profile/password").patch(verifyJwt, updatePassword); // updating the password of a user
-router.route("/profile/email").post(verifyJwt, createUpdateEmailOtp); // validate data and generate an OTP
-router.route("/profile/email/otp").patch(verifyJwt, updateEmail); // validate OTP and update the email
-router.route("/profile/delete").delete(verifyJwt, deleteUserAccount); // deleting the user
 
 export { router as userRouter };
