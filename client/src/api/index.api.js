dd8421056c741cb1fef4758ddb1de5a0c8df1d45
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-01-17 15:38:48 +0530

    Centralized the auth API exports

--- client/src/api/index.api.js
+++ client/src/api/index.api.js
@@ -2,3 +2,21 @@
 index.api.js
 Exporting every single API call 
 ------------------------------------------------------------------------------------------------- */
+
+import {
+  useRegisterOtpMutation,
+  useRegisterMutation,
+  useLoginMutation,
+  useLoginOtpMutation,
+  useLogoutMutation,
+  useNewTokenMutation,
+} from "./auth/authApi";
+
+export {
+  useRegisterOtpMutation,
+  useRegisterMutation,
+  useLoginMutation,
+  useLoginOtpMutation,
+  useLogoutMutation,
+  useNewTokenMutation,
+};
