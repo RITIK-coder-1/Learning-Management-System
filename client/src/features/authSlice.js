8e71644e1fe9f2a40ffe00b20773977ddbeebd9d
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-02-04 19:03:07 +0530

    I removed the user state from the auth redux slice

--- client/src/features/authSlice.js
+++ client/src/features/authSlice.js
@@ -8,16 +8,13 @@ import { createSlice } from "@reduxjs/toolkit";
 const authSlice = createSlice({
   name: "auth",
   initialState: {
-    user: null,
     isAuthenticated: false,
   },
   reducers: {
     setUser: (state, action) => {
-      state.user = action.payload;
       state.isAuthenticated = true;
     },
     disableUser: (state) => {
-      state.user = null;
       state.isAuthenticated = false;
     },
   },
