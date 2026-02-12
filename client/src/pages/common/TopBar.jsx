b7c244873d4d313ef4bcd8dce54053a5c00fb55a
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-02-12 13:43:25 +0530

    I created the basic boiler plate of the page top bar

---
+++ client/src/pages/common/TopBar.jsx
@@ -0,0 +1,17 @@
+/* ----------------------------------------------------------------------------------------------
+TopBar.jsx
+The static top bar for displaying the app logo and user profile 
+------------------------------------------------------------------------------------------------- */
+
+function TopBar() {
+  return (
+    <header>
+      {/* The Logo */}
+      <img />
+
+      {/* The profile pic of the user */}
+    </header>
+  );
+}
+
+export default TopBar;
