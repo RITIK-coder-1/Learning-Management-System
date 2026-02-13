dfbf88bb5e50eabf1a7cc9456820998216085655
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-02-13 16:34:50 +0530

    Added the functionality to display all the created courses of an instructor on the created-courses page

--- client/src/pages/course/CreatedCourses.jsx
+++ client/src/pages/course/CreatedCourses.jsx
@@ -3,13 +3,37 @@ CreatedCourses.jsx
 The page for displaying all the created courses of an instructor
 ------------------------------------------------------------------------------------------------- */
 
+import { useGetAllCoursesInstructorQuery } from "../../api/index.api";
 import { Navlink } from "../../components/index.components";
 
 function CreatedCourses() {
+  const { data } = useGetAllCoursesInstructorQuery();
+  console.log(data?.data);
+
+  const courses = data?.data?.map((course) => {
+    return {
+      id: crypto.randomUUID(),
+      title: course?.title,
+      desc: course?.description,
+      createdAt: course?.createdAt,
+    };
+  });
+
   return (
-    <Navlink to="/app/created-courses/create">
-      <button>Create</button>
-    </Navlink>
+    <>
+      <div className="flex flex-col p-2 gap-3">
+        {courses?.map((course) => {
+          return (
+            <div key={course.id} className="border">
+              {course.title}
+            </div>
+          );
+        })}
+      </div>
+      <Navlink to="/app/created-courses/create">
+        <button>Create</button>
+      </Navlink>
+    </>
   );
 }
 
