efe0c9a3e9427dd652e1ea3550bb0c6d507acbc2
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-02-13 14:42:25 +0530

    I added the navlink to the image at the top bar for navigating to the profile page

--- client/src/components/common/UserProfilePic.jsx
+++ client/src/components/common/UserProfilePic.jsx
@@ -4,17 +4,20 @@ The profile of the user at the top of the header
 ------------------------------------------------------------------------------------------------- */
 
 import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
 
 function UserProfilePic() {
   const user = useSelector((state) => state.auth.user);
 
   return (
     <div className="h-20 w-20">
-      <img
-        src={user?.profilePic}
-        alt="user profile pic"
-        className="h-full w-full"
-      />
+      <NavLink to="/app/profile">
+        <img
+          src={user?.profilePic}
+          alt="user profile pic"
+          className="h-full w-full"
+        />
+      </NavLink>
     </div>
   );
 }
