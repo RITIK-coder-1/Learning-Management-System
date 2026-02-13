eeb97b34968c06201c22e2012d4ce9d2c3ce8513
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-02-13 14:38:49 +0530

    Dynamically arranged the navigation sidebar based on the type of the user

--- client/src/components/layout/SideBar.jsx
+++ client/src/components/layout/SideBar.jsx
@@ -3,10 +3,13 @@ SideBar.jsx
 The static side bar for navigation 
 ------------------------------------------------------------------------------------------------- */
 
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 
 function SideBar() {
-  const navigationList = [
+  const user = useSelector((state) => state.auth.user);
+
+  const instructorNavigationList = [
     { path: "/app/dashboard", label: "Dashboard", id: crypto.randomUUID() },
     {
       path: "/app/enrolled-courses",
@@ -14,17 +17,33 @@ function SideBar() {
       id: crypto.randomUUID(),
     },
     { path: "/app/courses", label: "Explore", id: crypto.randomUUID() },
+    {
+      path: "/app/created-courses",
+      label: "Created Courses",
+      id: crypto.randomUUID(),
+    },
   ];
+
+  const userNavigationFilter = instructorNavigationList.filter(
+    (ele) => ele.label !== "Created Courses"
+  );
 
   return (
     <aside className="flex flex-col justify-around border">
       <nav>
         <ul>
-          {navigationList.map((ele) => (
-            <NavLink to={ele.path}>
-              <li key={ele.id}>{ele.label}</li>
-            </NavLink>
-          ))}
+          {user.accountType === "Instructor" &&
+            instructorNavigationList.map((ele) => (
+              <NavLink to={ele.path}>
+                <li key={ele.id}>{ele.label}</li>
+              </NavLink>
+            ))}
+          {user.accountType === "Student" &&
+            userNavigationFilter.map((ele) => (
+              <NavLink to={ele.path}>
+                <li key={ele.id}>{ele.label}</li>
+              </NavLink>
+            ))}
         </ul>
       </nav>
     </aside>
