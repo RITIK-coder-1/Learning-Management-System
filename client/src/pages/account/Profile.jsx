6ae64b2093c4c10564d2a862453643dd7e61bbfd
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-02-20 20:22:18 +0530

    Designed the other data of the user on the profile page for mobile screens

--- client/src/pages/account/Profile.jsx
+++ client/src/pages/account/Profile.jsx
@@ -10,6 +10,7 @@ function Profile() {
   const user = useSelector((state) => state.auth.user);
   console.log(user);
 
+  // The date of birth of the user
   const dob = user?.dateOfBirth;
   const date = new Date(dob);
   const year = date.getFullYear();
@@ -17,13 +18,42 @@ function Profile() {
   const day = date.getDate();
 
   return (
-    <section className="w-full h-full flex flex-col justify-start">
-      <div className="flex flex-col gap-2 justify-center items-center">
+    <section className="w-full h-full flex flex-col justify-start gap-5">
+      {/* The profile pic and Name */}
+      <div className="flex flex-col gap-3 justify-center items-center">
         <img
           src={user?.profilePic || "https://github.com/shadcn.png"}
           className="w-30 h-30 rounded-full shadow-lg shadow-black"
         />
-        <h1 className="text-3xl p-3 rounded-lg flex justify-center items-center border-b border-t border-white/10 font-black shadow-lg shadow-black bg-backgroundContrast">{user?.firstName} {user?.lastName}</h1>
+        <h1 className="text-3xl p-3 rounded-lg flex justify-center items-center border-b border-t border-white/10 font-black shadow-lg shadow-black bg-backgroundContrast">
+          {user?.firstName} {user?.lastName}
+        </h1>
+      </div>
+
+      <div className="w-full border border-white/10 p-2 flex flex-col gap-2">
+        {/* The email */}
+        <div className="flex flex-col border border-white/5 rounded-lg p-2">
+          <span className="text-lg text-yellow-50">{user?.email}</span>
+          <span className="text-xs text-foreground">Email</span>
+        </div>
+
+        {/* The username */}
+        <div className="flex flex-col border border-white/5 rounded-lg p-2">
+          <span className="text-lg text-yellow-50">{user?.username}</span>
+          <span className="text-xs text-foreground">Username</span>
+        </div>
+
+        {/* The DOB */}
+        <div className="flex flex-col border border-white/5 rounded-lg p-2">
+          <span className="text-lg text-yellow-50">{`${day}/${month}/${year}`}</span>
+          <span className="text-xs text-foreground">D.O.B</span>
+        </div>
+
+        {/* The Account Type */}
+        <div className="flex flex-col border border-white/5 rounded-lg p-2">
+          <span className="text-lg text-yellow-50">{user?.accountType}</span>
+          <span className="text-xs text-foreground">Type</span>
+        </div>
       </div>
 
       {/* <Navlink to="/app/profile/update-profile">
