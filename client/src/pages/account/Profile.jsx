eb7727ec34e4b1334e4b8e5c16ceecb2f075f532
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-02-20 19:59:00 +0530

    Designed the profile pic of the user on the profile page for mobile screens

--- client/src/pages/account/Profile.jsx
+++ client/src/pages/account/Profile.jsx
@@ -9,7 +9,7 @@ import { useSelector } from "react-redux";
 function Profile() {
   const user = useSelector((state) => state.auth.user);
   console.log(user);
-  
+
   const dob = user?.dateOfBirth;
   const date = new Date(dob);
   const year = date.getFullYear();
@@ -17,23 +17,16 @@ function Profile() {
   const day = date.getDate();
 
   return (
-    <>
-      <img
-        src={user?.profilePic || "https://github.com/shadcn.png"}
-        className="w-12 h-12"
-      />
-      <h1>
-        {user?.firstName} {user?.lastName}
-      </h1>
-      <span>{user?.username}</span>
-      <br />
-      <span>{user?.email}</span>
-      <br />
-      <span>
-        Date of birth: {day}/{month}/{year}
-      </span>
-      <br />
-      <Navlink to="/app/profile/update-profile">
+    <section className="w-full h-full flex flex-col justify-start">
+      <div className="flex flex-col gap-2 justify-center items-center">
+        <img
+          src={user?.profilePic || "https://github.com/shadcn.png"}
+          className="w-30 h-30 rounded-full shadow-lg shadow-black"
+        />
+        <h1 className="text-3xl p-3 rounded-lg flex justify-center items-center border-b border-t border-white/10 font-black shadow-lg shadow-black bg-backgroundContrast">{user?.firstName} {user?.lastName}</h1>
+      </div>
+
+      {/* <Navlink to="/app/profile/update-profile">
         <button>Update Profile</button>
       </Navlink>
       <Navlink to="/app/profile/update-password">
@@ -41,8 +34,8 @@ function Profile() {
       </Navlink>
       <Navlink to="/app/profile/update-email">
         <button>Update Email</button>
-      </Navlink>
-    </>
+      </Navlink> */}
+    </section>
   );
 }
 
