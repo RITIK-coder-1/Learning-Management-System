201248bc2799e211da9f7165f9aa3d161a225ce4
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-02-23 10:46:52 +0530

    Created the basic boilerplate of the explore courses page

--- client/src/pages/common/ExploreCourses.jsx
+++ client/src/pages/common/ExploreCourses.jsx
@@ -3,8 +3,43 @@ ExploreCourses.jsx
 The page for exploring various courses on the platform
 ------------------------------------------------------------------------------------------------- */
 
+import { useGetAllTheCoursesQuery } from "@/api/index.api";
+import filterCourses from "@/utils/filterCourses";
+import { CourseCard } from "../../components/index.components";
+
 function ExploreCourses() {
-  return <div>ExploreCourses</div>;
+  const { data } = useGetAllTheCoursesQuery(); // the course data
+
+  // the specific courses data to show on the page
+  const courses = filterCourses(data);
+
+  return (
+    <>
+      <h1 className="text-white text-4xl md:text-6xl">Courses</h1>
+      {courses?.length === 0 ? (
+        // Special label for no courses
+        <span className="text-foreground italic mt-5 md:text-lg">
+          There is no course on the platform yet.
+        </span>
+      ) : (
+        // the courses
+        <div className="w-full flex flex-col-reverse p-2 gap-5 justify-center items-center sm:flex-row-reverse">
+          {courses?.map((course) => {
+            return (
+              <CourseCard
+                image={course.img}
+                title={course.title}
+                description={course.desc}
+                price={course.price}
+                key={course.arrayId}
+                path={`/app/created-courses/${course.courseId}`}
+              />
+            );
+          })}
+        </div>
+      )}
+    </>
+  );
 }
 
 export default ExploreCourses;
