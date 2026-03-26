41829629ce770664fde5ec8a9a63ee69b0e103c3
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-03-26 12:33:05 +0530

    I wrote the logic to display the app sidebar only if the user is logged in

--- client/src/PrivateLayout.jsx
+++ client/src/PrivateLayout.jsx
@@ -6,18 +6,20 @@ It contains the routing for every single private page that requires authenticati
 import { TopBar, MainSection, AppSidebar } from "./components/index.components";
 import { Outlet } from "react-router-dom";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import useUserStatus from "./hooks/useUserStatus";
 
 function PrivateLayout() {
+  const { isAuthenticated } = useUserStatus();
   return (
     <>
       <TopBar />
 
       <SidebarProvider>
-        {/* The side bar for navigation */}
-        <AppSidebar />
+        {/* The side bar for navigation for logged in users */}
+        {isAuthenticated && <AppSidebar />}
 
         {/* The sidebar trigger */}
-        <SidebarTrigger className="fixed z-50" />
+        {isAuthenticated && <SidebarTrigger className="fixed z-50" />}
 
         {/* The main section of the page */}
         <MainSection>
