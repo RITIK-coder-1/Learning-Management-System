be6932c1b72b774600b67ff5741c526d75c32c30
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-04-01 13:13:01 +0530

    updated the environment variables settings for vite

--- client/src/api/base/apiSlice.js
+++ client/src/api/base/apiSlice.js
@@ -8,7 +8,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.RENDER_SERVER,
+    baseUrl: import.meta.env.VITE_RENDER_SERVER,
     credentials: "include",
     prepareHeaders: (headers) => {
       return headers;
